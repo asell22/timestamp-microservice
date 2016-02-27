3313489b7d0860cbf420a445892f05c69687b731
asell22 <alexanderjsell@gmail.com>
2016-02-27 01:31:30 +0000

    json returned depending on params

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
 var express = require('express');
 var app = express();
 
+var formatDate = function(date) {
+    var options = {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    }
+    
+    return date.toLocaleDateString('en-US', options);
+}
+
+var setTimeObj = function(date) {
+    var timeObj = {};
+    var unix = Date.parse(date) / 1000;
+    date = formatDate(date);
+    timeObj.natural = date;
+    timeObj.unix = unix;
+    
+    return timeObj;
+}
+
 app.use(express.static('public'));
 
 app.get('/:time', function(req, res) {
     var time = req.params.time;
-    var timeObj = {};
+    
     if (time * 1) {
-        console.log(new Date(time * 1000));
+        var date = new Date(time * 1000);
+        var timeObj = setTimeObj(date);
+        res.send(timeObj); 
     } else {
         if (new Date(time).toString() === "Invalid Date") {
-            console.log("Invalid");
+            res.send({
+                unix: null,
+                natural: null
+            });
         } else {
             var date = new Date(time);
-            console.log(date);
-            console.log(Date.parse(date) / 1000);
+            var timeObj = setTimeObj(date)
+            res.send(timeObj);
         }
     }
-    res.send();
 });
 
 app.listen(process.env.PORT, process.env.IP, function(req, res) {
